77ce6412667724809bc040cd808829596aaf10c5
Gary Kim <gary@garykim.dev>
2020-09-10 23:16:43 -0400

    Add reveal + intro to git

--- scripts/js.sh
+++ scripts/js.sh
@@ -7,5 +7,16 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 mkdir -p $SCRIPTPATH/../static/js/dist
 
+# viewer.js
 cp $SCRIPTPATH/../node_modules/viewerjs/dist/viewer.min.css $SCRIPTPATH/../static/js/dist/viewer.min.css
 cp $SCRIPTPATH/../node_modules/viewerjs/dist/viewer.min.js $SCRIPTPATH/../static/js/dist/viewer.min.js
+
+# reveal.js
+mkdir -p $SCRIPTPATH/../static/js/dist/reveal.js/plugins/markdown
+cp $SCRIPTPATH/../node_modules/reveal.js/plugin/markdown/markdown.js $SCRIPTPATH/../static/js/dist/reveal.js/plugins/markdown/markdown.js
+
+mkdir -p $SCRIPTPATH/../static/js/dist/reveal.js/plugins/highlight
+cp $SCRIPTPATH/../node_modules/reveal.js/plugin/highlight/highlight.js $SCRIPTPATH/../static/js/dist/reveal.js/plugins/highlight/highlight.js
+
+cp -r $SCRIPTPATH/../node_modules/reveal.js/dist $SCRIPTPATH/../static/js/dist/reveal.js/dist
+
