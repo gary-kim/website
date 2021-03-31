066cec972671cadeebaf07fd84e0564815a191b9
Gary Kim <gary@garykim.dev>
2021-03-31 18:03:27 -0400

    Add presentation
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- scripts/js.sh
+++ scripts/js.sh
@@ -18,5 +18,14 @@ cp $SCRIPTPATH/../node_modules/reveal.js/plugin/markdown/markdown.js $SCRIPTPATH
 mkdir -p $SCRIPTPATH/../static/js/dist/reveal.js/plugins/highlight
 cp $SCRIPTPATH/../node_modules/reveal.js/plugin/highlight/highlight.js $SCRIPTPATH/../static/js/dist/reveal.js/plugins/highlight/highlight.js
 
+mkdir -p $SCRIPTPATH/../static/js/dist/reveal.js/plugins/notes
+cp $SCRIPTPATH/../node_modules/reveal.js/plugin/notes/notes.js $SCRIPTPATH/../static/js/dist/reveal.js/plugins/notes/notes.js
+
+mkdir -p $SCRIPTPATH/../static/js/dist/reveal.js/plugins/zoom
+cp $SCRIPTPATH/../node_modules/reveal.js/plugin/zoom/zoom.js $SCRIPTPATH/../static/js/dist/reveal.js/plugins/zoom/zoom.js
+
+mkdir -p $SCRIPTPATH/../static/js/dist/reveal.js/plugins/search
+cp $SCRIPTPATH/../node_modules/reveal.js/plugin/search/search.js $SCRIPTPATH/../static/js/dist/reveal.js/plugins/search/search.js
+
 cp -r $SCRIPTPATH/../node_modules/reveal.js/dist $SCRIPTPATH/../static/js/dist/reveal.js/dist
 
